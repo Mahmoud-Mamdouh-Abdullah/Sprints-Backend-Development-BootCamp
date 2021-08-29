0c912243b6e3212bbbcc212fdc80f50b7c2d92c1
Mahmoud Mamdouh Abd Allah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2021-08-29 17:53:45 +0200

    UI updates

--- Chat App/script.js
+++ Chat App/script.js
@@ -1,5 +1,10 @@
 const loginSec = document.querySelector('#loginSec');
 const chatSec = document.querySelector('#chatSec');
+const msgArea = document.querySelector('#msgArea');
+const joinGroupBtn = document.querySelector('#joinGroup');
+const logoutBtn = document.querySelector('#logout');
+const sendMsgBtn = document.querySelector('#sendMsg');
+
 
 const joinGroup = function() {
     loginSec.classList.add('d-none');
@@ -13,5 +18,24 @@ const logout = function() {
 }
 
 const sendMsg = function() {
-    console.log("send");
+    console.log(msgArea.value, msgArea.value.length);
+    if(msgArea.value == null || msgArea.value.length === 0) {
+        alert('Enter your message first');
+        msgArea.value = '';
+        return;
+    }
+    msgArea.value = '';
 }
+
+joinGroupBtn.addEventListener('click', joinGroup);
+
+logoutBtn.addEventListener('click', logout);
+
+sendMsgBtn.addEventListener('click', sendMsg);
+
+msgArea.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+        msgArea.value = msgArea.value.replace(/[\r\n\v]+/g, '');
+        sendMsg();
+    }
+});
