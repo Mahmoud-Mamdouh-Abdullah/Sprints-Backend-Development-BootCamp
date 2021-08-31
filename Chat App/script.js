f14bb8b7e33143057975195acd61df8406c7d383
Mahmoud Mamdouh Abd Allah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2021-09-01 00:46:38 +0200

    pusher & chat logic

--- Chat App/script.js
+++ Chat App/script.js
@@ -7,13 +7,46 @@ const joinGroupBtn = document.querySelector('#joinGroup');
 const logoutBtn = document.querySelector('#logout');
 const sendMsgBtn = document.querySelector('#sendMsg');
 const timerP = document.querySelector('#timer');
+const groupNameP = document.querySelector('#groupNameP');
+const groupName = document.querySelector('#groupName');
+const myName = document.querySelector('#MyName');
 
 let count = 60;
 let timer;
 
+const subscribeChannel = function (channelName) {
+    Pusher.logToConsole = true;
+    var pusher = new Pusher('8783cde87284422cc978', {
+        cluster: 'mt1'
+    });
+
+    var channel = pusher.subscribe(channelName);
+    channel.bind('my-event', function (data) {
+        let dataObj = JSON.parse(JSON.stringify(data).replaceAll(`\\`, ''));
+        if (dataObj.sender !== myName.value) {
+            if (msgCounter === 0) {
+                msgDiv.innerHTML = '';
+            }
+            let msgBody =
+                `
+                <div class="msgBody d-flex flex-row justify-content-start mb-3">
+                    <div class="msgContent border d-flex flex-row p-2 justify-content-between">
+                     <p class="text-white text-start">${dataObj.sender} : ${dataObj.message}</p>
+                     </div>
+                </div>
+                 `;
+            msgDiv.insertAdjacentHTML('beforeend', msgBody);
+            msgDiv.scrollTop = msgDiv.scrollHeight;
+            msgCounter++;
+        }
+    });
+}
+
 const joinGroup = function () {
     loginSec.classList.add('d-none');
     chatSec.classList.remove('d-none');
+    groupNameP.innerText = groupName.value;
+    subscribeChannel(groupName.value);
     timer = setInterval(function () {
         count--;
         timerP.innerText = `You will logout after ${count} sec`
@@ -29,14 +62,45 @@ const logout = function () {
     chatSec.classList.add('d-none');
     count = 60;
     timerP.innerText = `You will logout after 60 sec`;
+    msgDiv.innerHTML = 
+            `
+            <div class="msgBody d-flex flex-row justify-content-center mb-3">
+                <div class="msgContent d-flex flex-row p-2 justify-content-center">
+                   <p>No Message yet</p>
+                </div>
+            </div>
+            `;
     clearInterval(timer);
+    msgCounter = 0;
+}
+
+const getMD5 = function (body) {
+    return CryptoJS.MD5(JSON.stringify(body));
+}
+
+const getAuthSignature = function (md5, timeStamp) {
+    return CryptoJS.HmacSHA256(`POST\n/apps/1258676/events\nauth_key=8783cde87284422cc978&auth_timestamp=${timeStamp}&auth_version=1.0&body_md5=${md5}`, "4863e6758db3f3075c2c");
+}
+
+let sendMessageToServer = async function (message, sender) {
+    let body = { data: `{"message":"${message}", "sender":"${sender}"}`, name: "my-event", channel: groupName.value }
+    let timeStamp = Date.now() / 1000;
+    let md5 = getMD5(body);
+    let url = `https://cors.bridged.cc/https://api-mt1.pusher.com/apps/1258676/events?body_md5=${md5}&auth_version=1.0&auth_key=8783cde87284422cc978&auth_timestamp=${timeStamp}&auth_signature=${getAuthSignature(md5, timeStamp)}`;
+    let req = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
 
 const addMsg = function () {
-    if(msgCounter === 0) {
+    if (msgCounter === 0) {
         msgDiv.innerHTML = '';
     }
-    let msgBody = 
+    let msgBody =
         `
         <div class="msgBody d-flex flex-row justify-content-end mb-3">
             <div class="msgContent border d-flex flex-row p-2 justify-content-between">
@@ -47,6 +111,8 @@ const addMsg = function () {
     msgDiv.insertAdjacentHTML('beforeend', msgBody);
     msgDiv.scrollTop = msgDiv.scrollHeight;
     msgCounter++;
+
+    sendMessageToServer(msgArea.value, myName.value);
 }
 
 const sendMsg = function () {
